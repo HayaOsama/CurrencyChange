f5fb4fb954689053491b861953e3245ed90d58af
HayaOsama <42942423+HayaOsama@users.noreply.github.com>
2021-01-01 17:21:34 +0200

    2nd commit

--- app/src/main/java/com/example/currencychange/View/MainActivity.java
+++ app/src/main/java/com/example/currencychange/View/MainActivity.java
@@ -13,10 +13,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.currencychange.R;
+import com.hbb20.CountryCodePicker;
 
 public class MainActivity extends AppCompatActivity {
   private   EditText fromCurrency , toCurrency ;
-  private   AppCompatSpinner fromSpinner , toSpinner ;
+  private CountryCodePicker fromSpinner , toSpinner ;
   private   ImageView convert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +29,14 @@ public class MainActivity extends AppCompatActivity {
         fromSpinner = findViewById(R.id.from_spinner);
         toSpinner = findViewById(R.id.to_spinner);
         convert = findViewById(R.id.convert);
-
         convert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-
+                String from = fromSpinner.getSelectedCountryNameCode();
+                String to = toSpinner.getSelectedCountryNameCode();
+                double amount = Double.parseDouble(fromCurrency.getText().toString());
+                double result = convert(from, to , amount);
+                toCurrency.setText(result+"");
             }
         });
 
@@ -49,5 +52,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         return super.onOptionsItemSelected(item);
+    }
+
+
+    public  double  convert(String from , String to , double amount){
+        return  1.0 ;
     }
 }// end class
